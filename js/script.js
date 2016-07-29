a2b2dbe89bac63cf74ddfcca322f1baeb4e3006c
John Cameron <john.n.cameron@gmail.com>
2016-07-29 12:26:37 +1000

    added setInterval

--- js/script.js
+++ js/script.js
@@ -32,4 +32,5 @@ function changeBackgroundColor() {
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote);
-
+// quote automatically updated every 30 seconds
+window.setInterval(printQuote, 30000);

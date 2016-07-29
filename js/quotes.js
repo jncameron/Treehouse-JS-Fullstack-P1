6680908773a2a60be720c230a9afb2653a0b743a
John Cameron <john.n.cameron@gmail.com>
2016-07-29 12:04:16 +1000

    For peer review

---
+++ js/quotes.js
@@ -0,0 +1,41 @@
+
+//'quotes' array contains quote information
+var quotes = [
+  {
+    quote: 'It is the mark of an educated mind to be able to entertain a thought without accepting it.',
+    source: 'Aristotle',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: 'I\'m sorry Dave, I\'m afraid I can\'t do that.',
+    source: 'HAL 9000',
+    citation: '2001: A Space Odyssey',
+    year: 1968
+  },
+  {
+    quote: 'Everything in moderation, including moderation.',
+    source: 'Oscar Wilde',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: 'Just taught my kids about taxes by eating 38% of their ice cream.',
+    source: 'Conan O\'Brien',
+    citation: 'Twitter',
+    year: 2011
+  },
+  {
+    quote: 'These aren\'t the droids you\'re looking for.',
+    source: 'Obi Wan Kenobi',
+    citation: '',
+    year: 1977
+  },
+  {
+    quote: 'People believe anything they see on the internet.',
+    source: 'Sir Winston Churchill',
+    citation: '',
+    year: 1942
+  }
+
+];
---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+
+// randomly chooses a quote from quotes array in quotes.js
+
+
+function getRandomQuote() {
+  var randomise = Math.floor(Math.random() * quotes.length);
+   return quotes[randomise];
+}
+
+//calls random quote from getRandomQuote and prints to 'quote-box' div
+function printQuote(quote) {
+  var chosenQuote = getRandomQuote();
+  var toQuote = '<p class="quote">' + chosenQuote.quote + '</p>';
+  toQuote += '<p class="source">' + chosenQuote.source;
+  //citation field only added to output if present
+  if (chosenQuote.citation.length !== 0) {        
+    toQuote += '<span class="citation">' + chosenQuote.citation + '</span>';
+  } 
+  //year field only added to output if present
+  if (chosenQuote.year.length !==0) {
+    toQuote += '<span class="year">' + chosenQuote.year + '</span></p>';
+  }
+  //console.log(chosenQuote);
+  var outputDiv = document.getElementById('quote-box');
+  outputDiv.innerHTML = toQuote;
+  changeBackgroundColor();
+}
+
+function changeBackgroundColor() {
+  colors = ['#3399ff', '#009933', '#3D003D', '#A3A300', '#669999', '#cc6600'];
+  randomColor = colors[Math.floor(Math.random() * colors.length)];
+  document.body.style.background = randomColor;
+}
+
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote);
+
